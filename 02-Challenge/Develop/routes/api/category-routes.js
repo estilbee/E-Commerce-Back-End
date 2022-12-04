58ca9777ef03766bb6f665e0a363453bb59525ea
estilbee <odaouphars@gmail.com>
2022-12-04 15:45:04 -0500

    updated routes

--- 02-Challenge/Develop/routes/api/category-routes.js
+++ 02-Challenge/Develop/routes/api/category-routes.js
@@ -3,26 +3,62 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-});
+router.get('/', async (req, res) => {
+    // DONE: ind all categories
+  const categoryData = await Category.findAll().catch((err) => {
+    res.json(err);
+  });
+  res.json(categoryData);
+  // TODO: be sure to include its associated Products
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
 });
+  
+router.get('/:id', async (req, res) => {
+  // DONE: find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id)
+      res.status(200).json(categoryData)
+    }
+    catch(err) {
+      res.status(400).json(err)
+    }
+    //TODO: be sure to include its associated Products
+    });
+  
 
 router.post('/', (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+router.put('/:id', async (req, res) => {
+  // DONE: update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    }) 
+    res.status(200).json(categoryData)
+  }
+  catch(err) {
+    res.status(400).json(err)
+  }
+  });
+  
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-});
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }) 
+    res.status(200).json(categoryData)
+  }
+  catch(err) {
+    res.status(400).json(err)
+  }
+  });
 
 module.exports = router;
